90bf0878123bb3a547bebd699f3015dce7b32fb2
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-01-20 00:30:21 +0900

    add installer.

---
+++ installer.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from pathlib import Path
+import subprocess
+
+if len(sys.argv) > 1:
+    install_path = Path(sys.argv[1])
+else:
+    install_path = Path('~/.pyviewer').expanduser()
+if install_path.exists():
+    print('{} already exists.'.format(install_path))
+    exit()
+
+subprocess.run(['git', 'clone', 'https://github.com/MeF0504/pyviewer.git', install_path])
+os.chdir(install_path)
+subprocess.run(['git', 'submodule', 'update', '--init'])
+
+if str(install_path/'bin') not in os.environ['PATH'].split(os.pathsep):
+    print('\nplease add {} to your PATH.'.format(install_path/'bin'))
