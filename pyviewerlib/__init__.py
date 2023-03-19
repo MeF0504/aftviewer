af7ee155dac7e50f63df19ffad7ca72bcca61948
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-03-19 20:29:02 +0900

    fail-safe if the curses module is not supported (Windows case).

--- pyviewerlib/__init__.py
+++ pyviewerlib/__init__.py
@@ -3,4 +3,12 @@ from .core import set_param, debug_print, args_chk, cprint, print_key, \
     interactive_view
 from .core.image_viewer import get_image_viewer, clear_mpl_axes, \
     get_exec_cmds, show_image_file, show_image_ndarray, is_image
-from .core.cui import interactive_cui
+try:
+    from .core.cui import interactive_cui
+except ImportError as e:
+    debug_print(e)
+
+    def interactive_cui(*args, **kwargs):
+        print('failed to import the interactive_cui function.')
+        print('maybe the curses module is not available.')
+        return
