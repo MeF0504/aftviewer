61d4e1b470e447b380b2ff486ed55f4af22f9dfa
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-03-28 10:01:54 +0900

    use the new version of pymeflib.tree.

--- pyviewerlib/hdf5.py
+++ pyviewerlib/hdf5.py
@@ -5,7 +5,7 @@ import h5py
 
 from . import args_chk, print_key, cprint, debug_print,\
     interactive_view, interactive_cui, FG, BG, END
-from pymeflib.tree import show_tree
+from pymeflib.tree2 import show_tree
 try:
     import numpy as np
 except ImportError:
@@ -47,24 +47,24 @@ def show_hdf5(h5_file, cpath, cui=False):
         if imp_np and is_array and (len(data) != 0):
             try:
                 dmean = np.nanmean(data)
+                res.append('mean : {}'.format(dmean))
             except Exception as e:
-                dmean = '{}: {}'.format(str(type(e)).split("'")[1], e)
-            res.append('mean : {}'.format(dmean))
+                debug_print('{}: {}'.format(str(type(e)).split("'")[1], e))
             try:
                 dmax = np.nanmax(data)
+                res.append(' max : {}'.format(dmax))
             except Exception as e:
-                dmax = '{}: {}'.format(str(type(e)).split("'")[1], e)
-            res.append(' max : {}'.format(dmax))
+                debug_print('{}: {}'.format(str(type(e)).split("'")[1], e))
             try:
                 dmin = np.nanmin(data)
+                res.append(' min : {}'.format(dmin))
             except Exception as e:
-                dmin = '{}: {}'.format(str(type(e)).split("'")[1], e)
-            res.append(' min : {}'.format(dmin))
+                debug_print('{}: {}'.format(str(type(e)).split("'")[1], e))
             try:
                 dstd = np.nanstd(data)
+                res.append(' std : {}'.format(dstd))
             except Exception as e:
-                dstd = '{}: {}'.format(str(type(e)).split("'")[1], e)
-            res.append(' std : {}'.format(dstd))
+                debug_print('{}: {}'.format(str(type(e)).split("'")[1], e))
             if hasattr(data, 'shape'):
                 try:
                     nan_rate = np.sum(np.isnan(data))/np.prod(data.shape)
@@ -74,53 +74,56 @@ def show_hdf5(h5_file, cpath, cui=False):
     return res, None
 
 
-def make_list(args, h5_file, list_tree, name, obj):
+def show_detail(h5_file, name, obj):
     if isinstance(obj, h5py.Dataset):
-        if not (args_chk(args, 'interactive') or args_chk(args, 'cui')):
-            if args_chk(args, 'verbose'):
-                print_key(name)
-                print("{}".format(h5_file[name][()]))
-            elif args_chk(args, 'key') and len(args.key) == 0:
-                print(name)
+        print_key(name)
+        print("{}".format(h5_file[name][()]))
 
-        tmp_list = list_tree
-        depth = 1
-        debug_print('cpath: {}'.format(name))
-        for p in name.split('/'):
-            if p == name.split('/')[-1] and depth == len(name.split('/')):
-                tmp_list.append(p)
-                debug_print('add {}'.format(p))
-            elif p in tmp_list[0]:
-                tmp_list = tmp_list[0][p]
-                depth += 1
-            else:
-                tmp_list[0][p] = [{}]
-                tmp_list = tmp_list[0][p]
-                depth += 1
+
+def show_names(name, obj):
+    if isinstance(obj, h5py.Dataset):
+        print(name)
+
+
+def get_contents(h5_file, root, path):
+    dirs = []
+    files = []
+    data = h5_file[str(path)]
+    if isinstance(data, h5py.Group):
+        for k in data.keys():
+            contents = '{}/{}'.format(path, k)
+            if isinstance(h5_file[contents], h5py.Group):
+                dirs.append(k)
+            elif isinstance(h5_file[contents], h5py.Dataset):
+                files.append(k)
+    return dirs, files
 
 
 def main(fpath, args):
-    list_tree = [{}]
     fname = os.path.basename(fpath)
 
     h5_file = h5py.File(fpath, 'r')
-    # make list_tree
-    h5_file.visititems(partial(make_list, args, h5_file, list_tree))
+    gc = partial(get_contents, h5_file)
 
     if args_chk(args, 'interactive'):
-        interactive_view(list_tree, fname, partial(show_hdf5, h5_file))
+        interactive_view(fname, gc, partial(show_hdf5, h5_file))
     elif args_chk(args, 'cui'):
-        interactive_cui(list_tree, fpath, partial(show_hdf5, h5_file))
+        interactive_cui(fpath, gc, partial(show_hdf5, h5_file))
     elif args_chk(args, 'key'):
-        for k in args.key:
-            print_key(k)
-            info, err = show_hdf5(h5_file, k, False)
-            if err is None:
-                print("\n".join(info))
-                print()
-            else:
-                cprint(err, fg='r')
-    elif not args_chk(args, 'verbose'):
-        show_tree(list_tree, fname)
+        if args.key:
+            for k in args.key:
+                print_key(k)
+                info, err = show_hdf5(h5_file, k, False)
+                if err is None:
+                    print("\n".join(info))
+                    print()
+                else:
+                    cprint(err, fg='r')
+        else:
+            h5_file.visititems(show_names)
+    elif args_chk(args, 'verbose'):
+        h5_file.visititems(partial(show_detail, h5_file))
+    else:
+        show_tree(fname, gc)
 
     h5_file.close()
