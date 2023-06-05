bc9faeff281b5e60379cd4e1f46331d833b5139e
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-06-05 17:27:21 +0900

    create a library to show help messages.

--- pyviewerlib/__init__.py
+++ pyviewerlib/__init__.py
@@ -6,6 +6,7 @@ from .core import debug_print, args_chk, cprint, print_key, get_col,\
     set_numpy_format, interactive_view, run_system_cmd, show_opts
 from .core.image_viewer import get_image_viewer, clear_mpl_axes, \
     get_exec_cmds, show_image_file, show_image_ndarray, is_image
+from .core.helpmsg import help_template
 try:
     from .core.cui import interactive_cui
 except ImportError as e:
