01e20f293780b64927e968e18d3781e17550f996
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-03-15 14:16:47 +0900

    Add some variables/dataclasses.

--- pyviewerlib/core/print_api_doc.py
+++ pyviewerlib/core/print_api_doc.py
@@ -7,7 +7,8 @@ sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 # NumPy Style -> https://numpydoc.readthedocs.io/en/latest/format.html
 
 funcs = {
-        '.core': ['args_chk', 'get_config', 'cprint', 'print_key',
+        '.core': ['GLOBAL_CONF', 'Args', 'ReturnMessage',
+                  'args_chk', 'get_config', 'cprint', 'print_key',
                   'get_col', 'set_numpy_format', 'show_opts',
                   'interactive_view', 'run_system_cmd',
                   ],
@@ -26,6 +27,7 @@ for mod, funcs in funcs.items():
     for func in funcs:
         ret = import_module(mod, 'pyviewerlib')
         ret2 = eval(f'ret.{func}')
-        print('## {}'.format(ret2.__name__))
+        # print('## {}'.format(ret2.__name__))
+        print('## {}'.format(func))
         print(ret2.__doc__)
         print()
