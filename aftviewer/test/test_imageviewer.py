b469e40b18706b44b031b5430edfd8f7941e76bb
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-10-20 18:11:25 +0900

    Fix a bug.

--- aftviewer/test/test_imageviewer.py
+++ aftviewer/test/test_imageviewer.py
@@ -73,8 +73,8 @@ def test_get_image_viewer_cui(iv):
         return
     image_viewer.__set_ImgViewer = False
     image_viewers = image_viewer.__collect_image_viewers()
-    __def_opts['defaults']['image_viewer_cui'] = iv
-    __user_opts['defaults'] = {}
+    __def_opts['config']['defaults']['image_viewer_cui'] = iv
+    __user_opts['config'] = {}
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='input file')
     parser.add_argument('--type')
@@ -108,8 +108,8 @@ def test_get_image_viewer_conf(iv):
         return
     image_viewer.__set_ImgViewer = False
     image_viewers = image_viewer.__collect_image_viewers()
-    __def_opts['defaults']['image_viewer'] = iv
-    __user_opts['defaults'] = {}
+    __def_opts['config']['defaults']['image_viewer'] = iv
+    __user_opts['config'] = {}
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='input file')
     parser.add_argument('--type')
@@ -137,8 +137,8 @@ def test_get_image_viewer_conf(iv):
 def test_get_image_viewer_search():
     # not set case
     image_viewer.__set_ImgViewer = False
-    __def_opts['defaults']['image_viewer'] = None
-    __user_opts['defaults'] = {}
+    __def_opts['config']['defaults']['image_viewer'] = None
+    __user_opts['config'] = {}
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='input file')
     parser.add_argument('--type')
