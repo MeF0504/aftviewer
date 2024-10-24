640d3187d2ab36aa91f7618551dc8b6e5c3eb1c1
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-10-24 16:53:59 +0900

    Add new option "-vv"
    which show more details of values.

--- aftviewer/viewers/numpy.py
+++ aftviewer/viewers/numpy.py
@@ -1,11 +1,23 @@
+import sys
+import shutil
+
 import numpy as np
 from numpy.lib.npyio import NpzFile
 
-from .. import args_chk, print_key, get_config, help_template, \
-    add_args_specification
+from .. import args_chk, print_key, get_config, help_template, add_args_key
+
+__detail_opts = dict(precision=None,
+                     threshold=sys.maxsize,
+                     edgeitems=None,
+                     linewidth=shutil.get_terminal_size().columns-2,
+                     suppress=True,
+                     nanstr='nan',
+                     infstr='inf',
+                     sign=' ',
+                     )
 
 
-def show_numpy(data: np.ndarray):
+def show_summary(data: np.ndarray):
     # shape
     dtype = data.dtype
     print('type     : {}'.format(dtype))
@@ -62,43 +74,72 @@ min      : {d_min}'''
 
 
 def add_args(parser):
-    add_args_specification(parser, verbose=True, key=True,
-                           interactive=False, cui=False)
+    ex_group = parser.add_mutually_exclusive_group()
+    add_args_key(ex_group)
+    ex_group.add_argument('-v', '--verbose',
+                          help='show details. -v just print the value'
+                          ' in this file/key.'
+                          ' -vv show details (mean, std, etc.) and'
+                          ' all numbers in this file/key.',
+                          action='count', default=0)
 
 
 def show_help():
     helpmsg = help_template('numpy',
                             'show the contents of a NumPy-compressed file.'
-                            ' If the file is "npz", you can specify the key name.',
+                            ' If the file is "npz",'
+                            ' you can specify the key name.',
                             add_args)
     print(helpmsg)
+
+
+def main_npy(data, args):
+    if hasattr(args, 'verbose') and args.verbose > 0:
+        if args.verbose == 1:
+            print(data)
+        elif args.verbose == 2:
+            with np.printoptions(**__detail_opts):
+                print(data)
+            show_summary(data)
+    elif args_chk(args, 'key'):
+        print("'-k' option is not supported in 'npy' file.")
+    else:
+        show_summary(data)
+
+
+def main_npz(data, args):
+    if hasattr(args, 'verbose') and args.verbose > 0:
+        if args.verbose == 1:
+            for k in data:
+                print_key(k)
+                print(data[k])
+        elif args.verbose == 2:
+            for k in data:
+                print_key(k)
+                with np.printoptions(**__detail_opts):
+                    print(data[k])
+                show_summary(data[k])
+    elif args_chk(args, 'key'):
+        if len(args.key) == 0:
+            for k in data:
+                print(k)
+        for k in args.key:
+            print_key(k)
+            print(data[k])
+            show_summary(data[k])
+            print()
+    else:
+        for k in data:
+            print()
+            print_key(k)
+            show_summary(data[k])
 
 
 def main(fpath, args):
     opts = get_config('numpy_printoptions')
     np.set_printoptions(**opts)
     data = np.load(fpath, allow_pickle=False)
-    if args_chk(args, 'verbose'):
-        if type(data) is NpzFile:
-            for k in data:
-                print_key(k)
-                print(data[k])
-        else:
-            print(data)
-    elif args_chk(args, 'key'):
-        if type(data) is NpzFile:
-            if len(args.key) == 0:
-                for k in data:
-                    print(k)
-            for k in args.key:
-                print_key(k)
-                print(data[k])
-                show_numpy(data[k])
-                print()
+    if type(data) is NpzFile:
+        main_npz(data, args)
     else:
-        if type(data) is NpzFile:
-            for k in data:
-                print('\n{}'.format(k))
-                show_numpy(data[k])
-        else:
-            show_numpy(data)
+        main_npy(data, args)
