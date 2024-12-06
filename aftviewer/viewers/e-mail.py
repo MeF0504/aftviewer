ad135932d6c5a85fd6ac7747792fa4a8d532c2ef
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-12-06 13:31:20 +0900

    Add args of -k and -v, and implement the process of -v.

--- aftviewer/viewers/e-mail.py
+++ aftviewer/viewers/e-mail.py
@@ -3,16 +3,22 @@ from pathlib import Path
 from email.parser import BytesParser
 from email import policy
 
-from aftviewer import Args, help_template
+from aftviewer import Args, help_template, print_key
 
 
 def add_args(parser: argparse.ArgumentParser) -> None:
-    parser.add_argument()
-    pass
+    parser.add_argument('-k', '--key',
+                        help='Specify the key of header info.',
+                        dest='key',
+                        nargs='*')
+    parser.add_argument('-v', '--verbose',
+                        help='Show all header names and its values.',
+                        dest='verbose',
+                        action='store_true')
 
 
 def show_help() -> None:
-    helpmsg = help_template('e-mail', 'Open and read e-mail (.eml) file.',
+    helpmsg = help_template('e-mail', 'Open and read e-mail file.',
                             add_args)
     print(helpmsg)
 
@@ -20,6 +26,8 @@ def show_help() -> None:
 def main(fpath: Path, args: Args):
     with open(fpath, 'rb') as f:
         msg = BytesParser(policy=policy.default).parse(f)
-    for key, val in msg.items():
-        print(f'----- {key} -----')
-        print(f'    {val}')
+
+    if args.verbose:
+        for key, val in msg.items():
+            print_key(key)
+            print(val)
