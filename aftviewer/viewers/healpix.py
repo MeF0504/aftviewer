f6885c3f5b494f06a706bb39e9eca61a44c941d7
Takuro Fujino <fujino.takuro.92@gmail.com>
2025-03-22 03:24:55 +0900

    Change the behavior of --key argument.

--- aftviewer/viewers/healpix.py
+++ aftviewer/viewers/healpix.py
@@ -3,9 +3,7 @@ from pathlib import Path
 from logging import getLogger
 
 import healpy as hp
-# healpy requires numpy, matplotlib, and astropy!
 import numpy as np
-from astropy.io import fits
 import matplotlib.pyplot as plt
 
 from pymeflib import plot as mefplot
@@ -18,9 +16,9 @@ logger = getLogger(GLOBAL_CONF.logname)
 
 def add_args(parser: argparse.ArgumentParser) -> None:
     add_args_key(parser,
-                 help='Specify the index of field/HDU. To see the details,'
-                 ' please run "aftviewer FILE -t fits -k".',
-                 default=None)
+                 help='Specify the index to show.'
+                      ' If no key is specified, show the list of map types.',
+                 type=int, default=None)
     parser.add_argument('--projection', help='specify the projection',
                         choices=['mollweide', 'gnomonic',
                                  'cartesian', 'orthographic'],
@@ -45,19 +43,7 @@ def show_help() -> None:
     print(helpmsg)
 
 
-def main(fpath: Path, args: Args):
-    fname = Path(fpath).name
-    if args_chk(args, 'key'):
-        if len(args.key) == 0:
-            with fits.open(fpath) as hdul:
-                hdul.info()
-            return
-        else:
-            fields = [int(k) for k in args.key]
-    else:
-        fields = None
-
-    # set projection
+def set_projection(args: Args) -> str:
     if hasattr(args, 'projection') and args.projection is not None:
         projection = args.projection
         logger.info(f'projection from args: {projection}')
@@ -67,8 +53,90 @@ def main(fpath: Path, args: Args):
     else:
         projection = 'mollweide'
         logger.info(f'projection from default: {projection}')
+    return projection
+
+
+def set_limits() -> tuple[float]:
+    try:
+        m_limit = get_config('map_limit')
+        mmin, mmax = m_limit
+    except Exception as e:
+        logger.error(f'failed to get map limits: {type(e).__name__}, {e}\n'
+                     f'  data: {m_limit}')
+        mmin = None
+        mmax = None
+    else:
+        del m_limit
+    return mmin, mmax
+
+
+def set_norm(args: Args) -> str | None:
+    if hasattr(args, 'norm') and args.norm is not None:
+        norm = args.norm
+        logger.info(f'norm from args: {norm}')
+    elif get_config('norm') is not None:
+        norm = get_config('norm')
+        logger.info(f'norm from config: {norm}')
+    else:
+        norm = 'None'
+        logger.info(f'norm from default: {norm}')
+    if norm == 'None':
+        norm = None
+    return None
+
+
+def set_coordinate(args: Args) -> list[str] | None:
+    if hasattr(args, 'coord') and args.coord is not None:
+        coord = args.coord
+        logger.info(f'coord from args: {coord}')
+    elif get_config('coord') is not None:
+        coord = get_config('coord')
+        logger.info(f'coord from config: {coord}')
+    else:
+        coord = []
+        logger.info(f'coord from default: {coord}')
+    if len(coord) == 0:
+        coord = None
+    return coord
+
+
+def show_header(fpath: Path):
+    _, headers = hp.read_map(fpath, h=True)
+    info = {}
+    for name, val in headers:
+        if 'TTYPE' in name:
+            idx = int(name[5:])
+            if idx not in info:
+                info[idx] = [val]
+            else:
+                logger.warning(f'idx overwrite? {idx}')
+                info[idx][0] = val
+        elif 'TUNIT' in name:
+            idx = int(name[5:])
+            if idx not in info:
+                logger.warning(f'idx not set? {idx}')
+                info[idx] = ['???', val]
+            else:
+                info[idx].append(val)
+    idcs = list(info.keys())
+    idcs.sort()
+    for i in idcs:
+        print(f'{i}: {info[i][0]} [{info[i][1]}]')
+
+
+def main(fpath: Path, args: Args):
+    fname = Path(fpath).name
+    if args_chk(args, 'key'):
+        if len(args.key) == 0:
+            show_header(fpath)
+            return
+        else:
+            idcs = args.key
+    else:
+        idcs = None
 
     # set viewer (visualization function)
+    projection = set_projection(args)
     if projection == 'mollweide':
         viewer = hp.mollview
     elif projection == 'gnomonic':
@@ -81,50 +149,14 @@ def main(fpath: Path, args: Args):
         print_error(f'incorrect projection: {projection}')
         return
 
-    # set max/min of map
-    try:
-        m_limit = get_config('map_limit')
-        mmin, mmax = m_limit
-    except Exception as e:
-        logger.error(f'failed to get map limits: {type(e).__name__}, {e}\n'
-                     f'  data: {m_limit}')
-        mmin = None
-        mmax = None
-    else:
-        del m_limit
-
-    # set normalization method
-    if hasattr(args, 'norm') and args.norm is not None:
-        norm = args.norm
-        logger.info(f'norm from args: {norm}')
-    elif get_config('norm') is not None:
-        norm = get_config('norm')
-        logger.info(f'norm from config: {norm}')
-    else:
-        norm = 'None'
-        logger.info(f'norm from default: {norm}')
-    if norm == 'None':
-        norm = None
-
-    # set coordinate
-    if hasattr(args, 'coord') and args.coord is not None:
-        coord = args.coord
-        logger.info(f'coord from args: {coord}')
-    elif get_config('coord') is not None:
-        coord = get_config('coord')
-        logger.info(f'coord from config: {coord}')
-    else:
-        coord = []
-        logger.info(f'coord from default: {coord}')
-    if len(coord) == 0:
-        coord = None
-
+    mmin, mmax = set_limits()
+    coord = set_coordinate(args)
+    norm = set_norm(args)
     # nest
     if hasattr(args, 'nest'):
         nest = args.nest
     else:
         nest = False
-
     # show C_\ell or not
     if hasattr(args, 'cl'):
         cl = args.cl
@@ -132,24 +164,21 @@ def main(fpath: Path, args: Args):
         cl = False
 
     # get and show maps
-    heal_maps, headers = hp.read_map(fpath, field=fields, h=True, nest=nest)
-    # get field names
-    logger.debug(f'fields: {fields}')
+    heal_maps, headers = hp.read_map(fpath, field=None, h=True, nest=nest)
     names = []
-    if fields is None:
-        for h in headers:
-            if 'TTYPE' in h[0]:
-                names.append(h[1])
-    else:
-        with fits.open(fpath) as hdul:
-            for i in fields:
-                names.append(hdul[i].name)
-        if len(fields) == 1:
-            heal_maps = [heal_maps]
+    for h in headers:
+        if 'TTYPE' in h[0]:
+            names.append(h[1])
     logger.debug(f'names: {names}')
-    assert len(names) == len(heal_maps)
-    Lmaps = len(heal_maps)
-    for i, name in enumerate(names):
+    assert len(names) == len(heal_maps), 'Number of names and maps' \
+        f' does not match: {len(names)}, {len(heal_maps)}.'
+    if idcs is None:
+        map_idcs = range(len(names))
+    else:
+        map_idcs = np.array(idcs)-1
+    Lmaps = len(map_idcs)
+    for i in map_idcs:
+        name = names[i]
         if norm == 'log':
             # is this correct?
             heal_map_plot = np.abs(heal_maps[i])
@@ -171,17 +200,17 @@ def main(fpath: Path, args: Args):
 
     if cl:
         fig2 = plt.figure(figsize=(10, 5))
-        if fields is None:
-            if Lmaps == 1:
-                # assume I map
-                cls = [hp.anafast(heal_maps[0], pol=False)]
-            else:
-                # assume I, Q, U map
-                cls = hp.anafast(heal_maps[:3], pol=True)
-                # to TT, EE, BB, TE, EB, TB
+        if Lmaps == 1:
+            # assume I map
+            cls = [hp.anafast(heal_maps[0], pol=False)]
+        elif Lmaps == 3:
+            # assume I, Q, U map
+            cls = hp.anafast(heal_maps[:3], pol=True)
+            # to TT, EE, BB, TE, EB, TB
         else:
-            cls = [hp.anafast(heal_maps[i], pol=False)
-                   for i in range(Lmaps)]
+            print_error(f'Number of maps should be 1 or 3. Now {Lmaps}.')
+            plt.show()
+            return
         Lcl = len(cls)
         ax2s = mefplot.share_plot(fig2, len(cls), 1)
         for i, cl in enumerate(cls):
