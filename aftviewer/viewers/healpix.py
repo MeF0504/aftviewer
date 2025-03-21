cf549083858a2e7069247795c29f5f1f75d72935
Takuro Fujino <fujino.takuro.92@gmail.com>
2025-03-21 17:17:03 +0900

    Update some settings and processes.
    But it looks has some problems yet...

--- aftviewer/viewers/healpix.py
+++ aftviewer/viewers/healpix.py
@@ -8,6 +8,7 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
+from pymeflib import plot as mefplot
 from aftviewer import (GLOBAL_CONF, Args,
                        args_chk, help_template, get_config, add_args_key,
                        print_error,
@@ -18,7 +19,8 @@ logger = getLogger(GLOBAL_CONF.logname)
 def add_args(parser: argparse.ArgumentParser) -> None:
     add_args_key(parser,
                  help='Specify the index of field/HDU. To see the details,'
-                 ' please run "aftviewer FILE -t fits -k".')
+                 ' please run "aftviewer FILE -t fits -k".',
+                 default=None)
     parser.add_argument('--projection', help='specify the projection',
                         choices=['mollweide', 'gnomonic',
                                  'cartesian', 'orthographic'],
@@ -51,15 +53,18 @@ def main(fpath: Path, args: Args):
         else:
             fields = [int(k) for k in args.key]
     else:
-        fields = [0]
+        fields = None
 
     # set projection
     if hasattr(args, 'projection') and args.projection is not None:
         projection = args.projection
+        logger.info(f'projection from args: {projection}')
     elif get_config('projection') is not None:
         projection = get_config('projection')
+        logger.info(f'projection from config: {projection}')
     else:
         projection = 'mollweide'
+        logger.info(f'projection from default: {projection}')
 
     # set viewer (visualization function)
     if projection == 'mollweide':
@@ -89,20 +94,26 @@ def main(fpath: Path, args: Args):
     # set normalization method
     if hasattr(args, 'norm') and args.norm is not None:
         norm = args.norm
+        logger.info(f'norm from args: {norm}')
     elif get_config('norm') is not None:
         norm = get_config('norm')
+        logger.info(f'norm from config: {norm}')
     else:
         norm = 'None'
+        logger.info(f'norm from default: {norm}')
     if norm == 'None':
         norm = None
 
     # set coordinate
     if hasattr(args, 'coord') and args.coord is not None:
         coord = args.coord
+        logger.info(f'coord from args: {coord}')
     elif get_config('coord') is not None:
         coord = get_config('coord')
+        logger.info(f'coord from config: {coord}')
     else:
         coord = []
+        logger.info(f'coord from default: {coord}')
     if len(coord) == 0:
         coord = None
 
@@ -112,16 +123,24 @@ def main(fpath: Path, args: Args):
     else:
         cl = False
 
-    # get field names
-    names = []
-    with fits.open(fpath) as hdul:
-        for i in fields:
-            names.append(hdul[i].name)
     # get and show maps
-    heal_maps = hp.read_map(fpath, field=fields)
-    if len(fields) == 1:
-        heal_maps = [heal_maps]
+    heal_maps, headers = hp.read_map(fpath, field=fields, h=True)
+    # get field names
+    logger.debug(f'fields: {fields}')
+    names = []
+    if fields is None:
+        for h in headers:
+            if 'TTYPE' in h[0]:
+                names.append(h[1])
+    else:
+        with fits.open(fpath) as hdul:
+            for i in fields:
+                names.append(hdul[i].name)
+        if len(fields) == 1:
+            heal_maps = [heal_maps]
+    logger.debug(f'names: {names}')
     assert len(names) == len(heal_maps)
+    Lmaps = len(heal_maps)
     for i, name in enumerate(names):
         if norm == 'log':
             # is this correct?
@@ -140,20 +159,32 @@ def main(fpath: Path, args: Args):
             tmpmax = mmax
         viewer(heal_map_plot, title=f'{fname} ({name})',
                min=tmpmin, max=tmpmax,
-               coord=coord, norm=norm, fig=1, sub=(len(fields), 1, i+1))
+               coord=coord, norm=norm, fig=1, sub=(Lmaps, 1, i+1))
 
     if cl:
         fig2 = plt.figure(figsize=(10, 5))
-        ax2s = [fig2.add_subplot(len(fields), 1, i+1)
-                for i in range(len(fields))]
-        for i, field in enumerate(fields):
-            # LMAX = 1024
-            cl = hp.anafast(heal_maps[i])  # , lmax=LMAX)
+        if fields is None:
+            if Lmaps == 1:
+                # assume I map
+                cls = [hp.anafast(heal_maps[0], pol=False)]
+            else:
+                # assume I, Q, U map
+                cls = hp.anafast(heal_maps[:3], pol=True)
+                # to TT, EE, BB, TE, EB, TB
+        else:
+            cls = [hp.anafast(heal_maps[i], pol=False)
+                   for i in range(Lmaps)]
+        Lcl = len(cls)
+        ax2s = mefplot.share_plot(fig2, len(cls), 1)
+        for i, cl in enumerate(cls):
             ell = np.arange(len(cl))
-            cl = cl[2:]
-            ell = ell[2:]
-            ax2s[i].plot(ell, ell*(ell+1)/(2*np.pi) * cl)
-            ax2s[i].set_xlabel(r'$\ell$')
-            ax2s[i].set_ylabel(r'$\ell(\ell+1)/2\pi\ C_{\ell}$')
+            dl = ell*(ell+1)/(2*np.pi)*cl
+            ax2s[i].plot(ell[2:], dl[2:], '-')
             # ax2s[i].set_yscale('log')
+            if i == int(Lcl/2):
+                ax2s[i].set_ylabel(r'$\ell(\ell+1)/2\pi\ C_{\ell}$')
+            if i == Lcl-1:
+                ax2s[i].set_xlabel(r'$\ell$')
+            else:
+                ax2s[i].set_xticklabels([])
     plt.show()
