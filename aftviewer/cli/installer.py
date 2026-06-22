b15cdc5d012025810b7d415796c4ad3e8b5224e4
Takuro Fujino <fujino.takuro.92@gmail.com>
2026-06-23 00:17:40 +0900

    check library contents

--- aftviewer/cli/installer.py
+++ aftviewer/cli/installer.py
@@ -11,8 +11,9 @@ from urllib import request
 from pathlib import Path
 from importlib import import_module
 from logging import getLogger
+from types import FunctionType
 
-from ..core import GLOBAL_CONF, print_error, print_warning, __add_lib2path
+from ..core import GLOBAL_CONF, print_error, print_warning, __load_lib
 
 logger = getLogger(GLOBAL_CONF.logname)
 
@@ -47,7 +48,14 @@ def install_viewer(args: argparse.Namespace) -> int:
             logger.error(f'remove {libdir}')
             shutil.rmtree(libdir)
             return res.returncode
-    return 0
+
+    res = 0
+    for fy in libdir.glob('viewers/*.py'):
+        res += check_viewer(fy.name[:-3])  # remove ".py"
+    if res == 0:
+        return 0
+    else:
+        return 3
 
 
 def update_viewer(libdir: Path) -> int:
@@ -60,10 +68,27 @@ def update_viewer(libdir: Path) -> int:
     return 0
 
 
+def check_viewer(typename: str) -> int:
+    libstr = f'viewers.{typename}'
+    logger.info(f'checking {libstr}')
+    try:
+        lib = import_module(libstr)
+    except ImportError as e:
+        print_warning(f'Failed to import {typename}: {e}')
+        return 1
 
-def check_viewer() -> int:
-    print('check viewer')
-    return 0
+    res = 0
+    if not (hasattr(lib, 'main') and type(lib.main) is FunctionType):
+        print_warning(f'main function is not in {typename}')
+        res += 1
+    if not (hasattr(lib, 'add_args') and type(lib.add_args) is FunctionType):
+        print_warning(f'add_args function is not in {typename}')
+        res += 1
+    if not (hasattr(lib, 'show_help') and type(lib.show_help) is FunctionType):
+        print_warning(f'show_help function is not in {typename}')
+        res += 1
+    return res
+
 
 def __install_viewer(args: argparse.Namespace, lib_path: Path):
     lib_dir = GLOBAL_CONF.conf_dir/'.lib/add_viewers'
